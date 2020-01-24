86c357f5e18bbaa62912ae2a9794fba2081c2fef
pankaj <pankajsh9472@gmail.com>
2020-01-24 13:41:59 +0530

    added toast function

--- app/src/main/java/com/example/bca/MainActivity.java
+++ app/src/main/java/com/example/bca/MainActivity.java
@@ -13,25 +13,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"hello dude ",Toast.LENGTH_SHORT).show();
     }
+    void toast(String msg)
+    {
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
+        toast("onPause");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        toast("ondestroy");;
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        toast("onRestart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        toast("onStop");
     }
 
 
